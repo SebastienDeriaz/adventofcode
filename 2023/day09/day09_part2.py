12c326f07cfb6b39807c455c811d9d0bdc4fc7b4
Sébastien Deriaz <71486197+SebastienDeriaz@users.noreply.github.com>
2023-12-09 16:06:52 +0100

    Day 9 - Part 2 (gg ez)

---
+++ 2023/day09/day09_part2.py
@@ -0,0 +1,39 @@
+from sys import argv
+
+
+def parse_sequences(data : list) -> list:
+    return [[int(x) for x in l.split(' ') if x != ''] for l in data]
+
+
+def find_next_value(sequence):
+    # Iterate over all sequences and create the pseudo-triangle 
+    sequences = [sequence]
+    while any([n != 0 for n in sequences[-1]]):
+        sequences.append([x1 - x0 for x0, x1 in zip(sequences[-1][:-1], sequences[-1][1:])])
+
+    # Add a new value to each row
+    previous_value = 0
+    for i, s in enumerate(sequences[::-1]):
+        new_value = previous_value + s[-1]
+        s.append(previous_value + s[-1])
+        previous_value = new_value
+    
+    #print(f"New sequence : {sequences[0]}")
+
+    return sequences[0][-1]
+            
+
+def main():
+    file = argv[1]
+
+    with open(file) as f:
+        data = f.readlines()
+        sequences = parse_sequences(data)   
+
+        # Just need to read the sequence backwards xD
+        print(sum([find_next_value(sequence[::-1]) for sequence in sequences]))
+
+
+if __name__ == '__main__':
+    main()
+        
