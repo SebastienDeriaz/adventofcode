ff3d4ee5270e2d2ed2235582912260bdbeee119c
Sébastien Deriaz <71486197+SebastienDeriaz@users.noreply.github.com>
2023-12-12 05:55:12 +0100

    Day 5 - Part 2 (finally !)

--- 2023/day05/day05_part2.py
+++ 2023/day05/day05_part2.py
@@ -1,147 +1,176 @@
-# No need to create dictionaries, a simple start + offset will do
-# if the value is >= start, add offset, otherwise keep it
-# There can be multiple ranges
+# Start over
 from sys import argv
 import re
 
-# Each variables[x] can be mapped to variables[x+1]
+
 variables = [
-    'seed',
-    'soil',
-    'fertilizer',
-    'water',
-    'light',
-    'temperature',
-    'humidity',
-    'location'
+    'seed-to-soil map',
+    'soil-to-fertilizer map',
+    'fertilizer-to-water map',
+    'water-to-light map',
+    'light-to-temperature map',
+    'temperature-to-humidity map',
+    'humidity-to-location map'
 ]
 
+class Map:
+    def __init__(self, dest : int, src : int, length : int) -> None:
+        self._dest = dest
+        self._src = src
+        self._len = length
+        self._src_end = self._src + self._len - 1
+        self._offset = self._dest - self._src
+    
+    def __repr__(self) -> str:
+        return f'{self._src}:>{self._src_end}:{self._offset:+n}'
+
+    def offset(self):
+        return self._offset
+
+    def start(self):
+        return self._src
+
+    def end(self):
+        return self._src_end
+
 class Range:
-    def __init__(self, start, length) -> None:
+    def __init__(self, start : int, length : int = None, end : int = None) -> None:
         self._start = start
-        self._length = length
-        pass
+        if end is not None:
+            self._end = end
+            self._length = self._end - self._start + 1 
+        elif length is not None:
+            self._end = self._start + length - 1
+            self._length = length
+        else:
+            raise RuntimeError("Either end or length must be defined")
 
     def __repr__(self) -> str:
-        return f"{self._start}->{self._start+self._length-1}"
-
-    def __str__(self) -> str:
-        return self.__repr__()
+        return f'{self._start}->{self._end}'
 
     def start(self):
         return self._start
     
     def end(self):
-        return self._start + self._length - 1
+        return self._end
 
-class Map:
-    def __init__(self, destination, source, length) -> None:
-        self._destination = destination
-        self._source = source
-        self._length = length
-        self._offset = self._destination - self._source
+    def length(self):
+        return self._length
 
-    def __repr__(self) -> str:
-        return f"{self._source}->{self._source+self._length-1}:{self._offset:+d}"
+    def contains(self, x):
+        return self.start() <= x <= self.end()
 
-    def __str__(self) -> str:
-        return self.__repr__()
+    def __eq__(self, __o: object) -> bool:
+        return __o.start() == self.start() and __o.end() == self.end()
 
-    def start(self):
-        return self._source
-    
-    def end(self):
-        return self._source + self._length - 1
+    def apply_maps(self, maps : list) -> list:
+        # Apply all maps first
+        ranges = []
+        source_mapped_ranges = []
+        for _map in maps:
+            b_start = max(self.start(), _map.start())
+            b_end = min(self.end(), _map.end())
+            if b_end >= b_start:
+                nr = Range(start=b_start+_map.offset(), end=b_end+_map.offset())
+                original = Range(start=b_start, end=b_end)
+                source_mapped_ranges.append(original)
+                ranges.append(nr)
 
-    def offset(self):
-        return self._offset
+        # Add all the missing ranges
+        source_mapped_ranges = reduce(source_mapped_ranges)
+        source_mapped_ranges.sort(key=lambda x : x.start())
+        
+        start = self.start()
+        r : Range
+        if len(source_mapped_ranges) == 0:
+            # Add everything
+            ranges.append(self)
+        else:
+            start = self.start()
+            for r in source_mapped_ranges:
+                end = r.start() - 1
+                if end >= start:
+                    # Add this range
+                    nr = Range(start=start, end=end)
+                    ranges.append(nr)
+                start = r.end() + 1
+            end = self.end()
+            if end >= start:
+                nr = Range(start=start, end=end)
+                ranges.append(nr)
 
-def apply_map_to_range(_map : Map, _range : Range) -> list:
-    # Create a single smaller or equal map
-    start = max([_map.start(), _range.start()])
-    end = min([_map.end(), _range.end()])
-    length = end - start + 1
-    if length > 0:
-        # There is a range !
-        return (Range(start, length), Range(start+_map.offset(), length))
+        ranges.sort(key=lambda x : x.start())
 
+        assert sum([r.length() for r in ranges]) == self.length()
 
-def apply_maps_to_range(maps : list, _range : Range):
-    print()
-    print(f'Apply {maps} to {_range}')
-    map_ranges = []
-    maps.sort(key=lambda x : x.start())
-    for _map in maps:
-        output = apply_map_to_range(_map, _range)
-        if output is not None:
-            map_ranges.append(output)
+        return ranges
 
-    # Add the missing ranges
-    start = _range.start()
-    output_ranges = []
-    original_range = None
-    for original_range, mapped_range in sorted(map_ranges, key=lambda x : x[0].start()):
-        if original_range.start() > start:
-            # Add a non-map range
-            output_ranges.append((Range(start, original_range.start() - start), Range(start, original_range.start() - start)))
-        # Add the current map range
-        output_ranges.append((original_range, mapped_range))
-        start = mapped_range.end() + 1
-    if original_range is None:
-        # No map-range at all, return the original range
-        output_ranges.append((_range, _range))
-    elif original_range.end() < _range.end():
-        # Add the final non-map range
-        _rng = Range(original_range.end()+1, _range.end() - original_range.end())
-        output_ranges.append((_rng, _rng))
+    def overlap(self, range_2) -> bool:
+        return self.start() <= range_2.start() <= self.end() or \
+            range_2.start() <= self.start() <= range_2.end() 
 
-    output = sorted([x[1] for x in output_ranges], key=lambda x : x.start())
-    #assert output[0].start() > 0
-    return output
+    def __or__(self, range_2):
+        return Range(start=min(self.start(), range_2.start()), end=max(self.end(), range_2.end()))
 
 
+def reduce(ranges : list) -> list:
+    r1 : Range
+    r2 : Range
 
-def parse_maps(data):
-    pattern = '(\w+)-to-(\w+) map:\n((?:[\w ]+\n)+)'
-    # Map from x to the next one
-    maps = {}
-    for a, _, _maps in re.findall(pattern, data):
-        map_blocks = [Map(*[int(y) for y in x.split(' ') if y != '']) for x in _maps.split('\n') if x != '']
-        maps[a] = map_blocks
-    return maps
+    new_ranges = []
+    exclude = []
+    for i1, r1 in enumerate(ranges):
+        if i1 in exclude:
+            continue
+        new_range = r1
+        for i2, r2 in enumerate(ranges):
+            if new_range.overlap(r2):
+                new_range |= r2
+                exclude.append(i2)
+        new_ranges.append(new_range)
+    return new_ranges
 
-def parse_seeds(data):
-    # Return the list of seeds
-    pattern = 'seeds:([ \w]+)'
-    seeds_str = re.match(pattern, data).groups()
-    seeds_ranges = [int(x) for x in seeds_str[0].split(' ') if x != '']
-    seeds = [Range(start, length) for start, length in zip(seeds_ranges[::2], seeds_ranges[1::2])]
-    return seeds
+def parse_categories(data):
+    pattern = '(?:([\w -]+):[ \n])([\d\n ]+)'
+    categories = re.findall(pattern, data)
+    return {name : [[int(x) for x in line.split(' ') if x != ''] for line in values.split('\n') if line != ''] for name, values in categories}
+
+
+def seed_ranges(seeds : list) -> list:
+    return [Range(start=s, length=l) for s, l in zip(seeds[::2], seeds[1::2])]
+
 
 def apply_maps_to_ranges(maps : list, ranges : list):
-    _ranges = []
+    new_ranges = []
+    # Apply maps to each range
+    _range : Range
     for _range in ranges:
-        _ranges += apply_maps_to_range(maps, _range)
-    return sorted(_ranges, key=lambda x : x.start())
-        
+        new_ranges += _range.apply_maps(maps)
+    #print(f'Ranges after map (unreduced) : {new_ranges}')
+    #new_ranges = reduce(new_ranges)
+    #print(f'Ranges after map : {new_ranges}')
+    return new_ranges
+
+
 def main():
     file = argv[1]
     with open(file) as f:
         data = f.read()
-        seeds = parse_seeds(data)
-        variable_maps = parse_maps(data)
+        categories = parse_categories(data)
+        seeds = seed_ranges(categories['seeds'][0])
+        maps = [[Map(*m) for m in categories[v]] for v in variables]
 
         ranges = seeds
-        #for v in variables[:-1]:
-        for v in variables[:-1]:
-            print(f"###################### Apply variable {v} ####################################")
-            ranges = apply_maps_to_ranges(variable_maps[v], ranges)
+        for _map in maps:
+            # V is the source variable
+            ranges = apply_maps_to_ranges(_map, ranges)
 
-        print(ranges)
+        
 
-        #print([x2.start() - x1.start() for x1, x2 in zip(ranges[:-1], ranges[1:])])
+        #print(min([r.start() for r in ranges]))
 
-        print(ranges[0].start())
+
+
 
 
 if __name__ == '__main__':
