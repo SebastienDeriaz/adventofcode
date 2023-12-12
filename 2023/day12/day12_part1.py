7a0893063e8a14ef45239d4bbdd1db0d2e32abdd
SebastienDeriaz <71486197+SebastienDeriaz@users.noreply.github.com>
2023-12-12 20:23:26 +0100

    Day12 - Part 1

--- 2023/day12/day12_part1.py
+++ 2023/day12/day12_part1.py
@@ -1,4 +1,5 @@
 from sys import argv
+from tqdm import tqdm
 
 
 BROKEN = '#'
@@ -17,8 +18,6 @@ def count_conditions(conditions):
     return [len(x) for x in conditions.split(OPERATIONAL) if x != '']
 
 def arangements(conditions : str, counts : list):
-    # First split in groups
-    print(f'Line : {conditions}, {counts}')
     # Try out all possibilities
     N = 0
     N_unknowns = conditions.count(UNKNOWN)
@@ -26,34 +25,30 @@ def arangements(conditions : str, counts : list):
         bits = [int(x) for x in f'{i:0{N_unknowns}b}']
         b_counter = 0
         new_conditions = ''
-        for i, c in conditions:
-            
-            new_conditions += str(y)
+        for i, c in enumerate(conditions):
+            if c == UNKNOWN:
+                if bits[b_counter]:
+                    new_conditions += OPERATIONAL
+                else:
+                    new_conditions += BROKEN
+                b_counter += 1
+            else:
+                new_conditions += c
 
-            new_conditions = ''.join([(OPERATIONAL if b > 0 else BROKEN) if conditions[i] == UNKNOWN else c  for i, c in enumerate(conditions)])
-
-        print(f'{new_conditions} : {count_conditions(new_conditions)}')
         if count_conditions(new_conditions) == counts:
-            print(f'  add')
             N += 1
-
-    print(f'  ->{N}')
     return N
-
-
-
-    
-
-
-
 
 
 def main():
     file = argv[1]
     with open(file) as f:
         lines = f.readlines()
-        parsed_line = parse_line(lines[0])
-        arangements(*parsed_line)
+        N = 0
+        for line in tqdm(lines):
+            parsed_line = parse_line(line)
+            N += arangements(*parsed_line)
+        print(N)
 
         #conditions_counts = [parse_line(line) for line in lines if line != '']
         #print(conditions_counts)
