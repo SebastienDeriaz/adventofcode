6607847f603ad35b4a6076d6d89b8cef2f13c74d
Sébastien Deriaz <71486197+SebastienDeriaz@users.noreply.github.com>
2023-12-13 17:14:48 +0100

    Day 10 - Pat 2

--- 2023/day10/day10_part2_bis.py
+++ 2023/day10/day10_part2_bis.py
@@ -1,6 +1,7 @@
 from sys import argv
 import numpy as np
 from enum import Enum
+import matplotlib.pyplot as plt
 
 
 NORTH = (1 << 0) # 1
@@ -39,17 +40,24 @@ def propagate(matrix, position, val):
         (1, 0),
         (-1, 0)
     ]
-    i = 1
-    print(f'propagate {val} to {position}')
-    matrix[position[::-1]] = val
-    for offset in offsets:
-        new_position = position_add(position, offset)
-        if 0 <= new_position[0] < matrix.shape[1] and 0 <=§ new_position[1] < matrix.shape[0]:
-            if matrix[new_position[::-1]] == GROUND:
-                i += propagate(matrix, new_position, val)
-
-    # Avoid recursion
-
+    positions = [position]
+    neighbors = []
+    stop = False
+    i = 0
+    while not stop:
+        stop = True
+        for p in positions:
+            matrix[p[::-1]] = val
+            i += 1
+            for offset in offsets:
+                new_position = position_add(p, offset)
+                if 0 <= new_position[0] < matrix.shape[1] and 0 <= new_position[1] < matrix.shape[0]:
+                    if matrix[new_position[::-1]] == GROUND:
+                        if new_position not in neighbors:
+                            neighbors.append(new_position)
+                        stop = False
+        positions = neighbors
+        neighbors = []
     return i
 
 
@@ -59,12 +67,17 @@ def position_add(x1, x2):
 def position_substract(x1, x2):
     return x1[0] - x2[0], x1[1] - x2[1]
 
+def position_rotation_clockwise(x):
+    return x[1], -x[0]
+
+def position_rotation_counterclockwise(x):
+    return -x[1], x[0]
+
 
 def find_start_pipes(matrix):
     # Find the start
     yx = np.array(np.where(matrix == STARTING_POSITION))
     # Find the start pipes
-    print(f'Start at ({yx[1,0]}, {yx[0,0]})')
     positions = []
     directions = []
     for dx, dy in XY_TO_DIRECTION:
@@ -92,7 +105,6 @@ def parse_matrix(data) -> np.array:
     data_mapped = [[SYMBOLS[x] for x in line] for line in data.split('\n')]
     return np.array(data_mapped)
 
-
 def find_loop(matrix):
     # Find the starting point
     start, positions, directions = find_start_pipes(matrix)
@@ -103,7 +115,6 @@ def find_loop(matrix):
     _continue = True
     loop[start[::-1]] = 1
     while _continue:
-        print(positions)
         loop[position[::-1]] = 1
         position, direction = iterate(matrix, position, direction)
         if direction == 0:
@@ -112,32 +123,46 @@ def find_loop(matrix):
 
     return loop
 
+def propagate_adjacent(matrix, position, direction):
+    xy = DIRECTION_TO_XY[direction]
+    xy_rot = position_rotation_counterclockwise(xy)
+    on_the_left = position_add(position, xy_rot)
+    N = [0, 0]
+    if 0 <= on_the_left[0] < matrix.shape[1] and \
+        0 <= on_the_left[1] < matrix.shape[0] and \
+                matrix[on_the_left[::-1]] == GROUND:
+        N[0] = propagate(matrix, on_the_left, LEFT)
+    on_the_right = position_substract(position, xy_rot)
+    if 0 <= on_the_right[0] < matrix.shape[1] and \
+        0 <= on_the_right[1] < matrix.shape[0] and \
+                matrix[on_the_right[::-1]] == GROUND:
+        N[1] = propagate(matrix, on_the_right, RIGHT)
+    return N
+
 def count_enclosed(matrix, loop):
     filtered_matrix = matrix.copy()
     filtered_matrix[loop == 0] = GROUND
     # Find the starting point
-    start, positions, directions = find_start_pipes(filtered_matrix)
+    _, positions, directions = find_start_pipes(filtered_matrix)
     N = [0, 0]
     direction = directions[0]
     position = positions[0]
     _continue = True
     while _continue:
-        if direction in [NORTH, SOUTH]:
-            xy = DIRECTION_TO_XY[direction]
-            on_the_left = position_substract(position, xy[::-1])
-            if 0 <= on_the_left[0] < filtered_matrix.shape[1] and filtered_matrix[on_the_left[::-1]] == GROUND:
-                N[0] += propagate(filtered_matrix, on_the_left, LEFT)
-            on_the_right = position_add(position, xy[::-1])
-            if 0 <= on_the_right[0] < filtered_matrix.shape[1] and filtered_matrix[on_the_right[::-1]] == GROUND:
-                N[1] += propagate(filtered_matrix, on_the_right, RIGHT)
+        Nnew = propagate_adjacent(filtered_matrix, position, direction)        
+        N[0] += Nnew[0]
+        N[1] += Nnew[1]
 
+        old_direction = direction
         position, direction = iterate(filtered_matrix, position, direction)
-        
+
+        Nnew = propagate_adjacent(filtered_matrix, position, old_direction)
+        N[0] += Nnew[0]
+        N[1] += Nnew[1]
         
         if direction == 0:
             _continue = False
-
-    return N, filtered_matrix
+    return N
 
 def main():
     file = argv[1]
