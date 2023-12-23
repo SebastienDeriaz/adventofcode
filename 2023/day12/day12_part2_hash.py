3ce957bfe44d68b039dfcb4c46e6e62191d63db7
SebastienDeriaz <71486197+SebastienDeriaz@users.noreply.github.com>
2023-12-23 10:59:22 +0100

    sync

--- 2023/day12/day12_part2_hash.py
+++ 2023/day12/day12_part2_hash.py
@@ -59,7 +59,11 @@ def _hash(N : int, positions : list, counts : list):
 def disp_hash(N, hash):
     return f'{hash:0{N}b}'[::-1]
 
+call_counter = 0
+
 def try_all(N : int, broken_hash : int, operational_hash : int, positions, counts, i):
+    global call_counter
+    call_counter += 1
     if i == 0:
         _min = 0
     else:
@@ -91,6 +95,7 @@ def try_all(N : int, broken_hash : int, operational_hash : int, positions, count
 
             if i == len(positions) - 1:
                 counter += 1
+                #print(disp_hash(N, new_hash).replace('0', '.').replace('1', '#'))
             else:
                 counter += try_all(N, broken_hash, operational_hash, positions, counts, i+1)
 
@@ -124,16 +129,33 @@ UNFOLD_FACTOR = 5
 
 def main():
     global perf_hist
+    global call_counter
     file = argv[1]
     with open(file) as f:
         lines = f.readlines()
         N = 0
-        for line in tqdm(lines):
+        for i, line in enumerate(lines[:15]):
+            print(f'Line {i+1}')
             parsed_line = parse_line(line)
+
+
             
-            unfolded_line = unfold(*parsed_line, UNFOLD_FACTOR)
-            #print(f"Line : {unfolded_line}")
-            n = count_arangements(*unfolded_line)
+
+            previous_value = 1
+            previous_expanded = 1
+            for uf in range(1, UNFOLD_FACTOR+1):
+                unfolded_line = unfold(*parsed_line, uf)
+                expanded_line = (unfolded_line[0] + UNKNOWN, unfolded_line[1])
+                n = count_arangements(*unfolded_line)
+                n_expanded = count_arangements(*expanded_line)
+                if uf > 1:
+                    print(f' {uf} : {n}/({n_expanded}) x{n / previous_value:.2f}/(x{n_expanded / previous_expanded:.2f}) ')
+                else:
+                    print(f' {uf} : {n} ({n_expanded})')
+
+                previous_value = n
+                previous_expanded = n_expanded
+                
             N += n
 
             #print(f'Line {il+1} {parsed_line}: {n1}, {n2}, {n3}, {n4}')
@@ -141,6 +163,7 @@ def main():
             #N += nuf
 
         print(f'Total : {N}')
+        print(f'{call_counter=}')
         #print(f'mean : {np.mean(perf_hist)*1e-3:.1f} us, std : {np.std(perf_hist)*1e-3:.1f} us')
 
 
