2753e9b22c8275966692e1d88862bd2089d513f6
Sébastien Deriaz <71486197+SebastienDeriaz@users.noreply.github.com>
2024-12-08 08:34:28 +0100

    Day 7

---
+++ 2024/day07/day07_1.py
@@ -0,0 +1,51 @@
+import sys
+import re
+from itertools import product
+from primefac import primefac
+from enum import Enum
+
+def parse_file(data):
+    PATTERN = '(\d+):([\d+ ]+)'
+    equations = []
+    for line in data.split('\n'):
+        value, numbers = re.match(PATTERN, line).groups()
+        equations.append((int(value), [int(x) for x in filter(None, numbers.split(' '))]))
+
+    return equations
+
+class Operator(Enum):
+    ADD = '+'
+    MUL = '*'
+
+def print_equation(numbers, operators, result):
+    print(f'{numbers[0]} {" ".join([f"{o.value} {n}" for o, n in zip(operators, numbers[1:])])} = {result}')
+    
+def sum_valid_equations_results(equations):
+    output = 0
+    for value, numbers in equations:
+        for operators in product([Operator.ADD, Operator.MUL], repeat=len(numbers)-1): 
+            estimation = numbers[0]
+            for n, operator in zip(numbers[1:], operators):
+                if operator == Operator.ADD:
+                    estimation += n
+                elif operator == Operator.MUL:
+                    estimation *= n
+            if estimation == value:
+                output += value
+                break
+
+    return output
+
+def main():
+    file = sys.argv[1]
+
+
+    with open(file) as f:
+        data = f.read()
+
+        equations = parse_file(data)
+
+        print(sum_valid_equations_results(equations))
+
+if __name__ == '__main__':
+    main()
