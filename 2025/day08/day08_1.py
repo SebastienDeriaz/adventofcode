18824fb7a286c234f8b00282505387da4956fa21
Sébastien Deriaz <sebastien.deriaz1@gmail.com>
2025-12-08 21:47:04 +0100

    Day 8

--- 2025/day08/day08_1.py
+++ 2025/day08/day08_1.py
@@ -2,14 +2,11 @@ from sys import argv
 from typing import Dict
 import numpy as np
 import time
-from collections import Counter
-
-def pair(a : int, b : int):
-    return f'{a}-{b}'
 
 def main():
     file = argv[1]
-    N = int(argv[2])
+    N_connections = int(argv[2])
+    N_largest = int(argv[3])
 
     positions = []
     with open(file) as f:
@@ -20,47 +17,22 @@ def main():
 
     positions = np.array(positions)
 
-    #print(positions)
-    distance_pairs = []
-    #distance_pairs_match = {}
     pairs = []
 
-    start = time.time()
-    for i, p in enumerate(positions):
-        distances = np.linalg.norm(positions - p, axis=1)
-        distances[i] = float('inf')
-        shortest = int(np.where(distances == distances.min())[0][0])
+    k = np.arange(positions.shape[0], dtype=int)
 
-        pa = pair(i, shortest)
-        if (pa not in pairs) and (pair(shortest, i) not in pairs):
-            
-            pairs.append(pa)
-            distance_pairs.append((i, shortest, float(distances.min())))
+    #start = time.time()
+    for a, p in enumerate(positions):
+        distances = np.linalg.norm(positions[a+1:,:] - p, axis=1)
+        ki = k[a+1:]
+        #distances[i] = float('inf')
 
-        # if (shortest in distance_pairs_match) and (distance_pairs[distance_pairs_match[shortest]][1] == i):
-        #     distance_pairs_match[i] = distance_pairs[distance_pairs_match[shortest]][1]
-        # else:
-        #     distance_pairs_match[i] = len(distance_pairs)-1
-        #distance_pairs_match[shortest] = len(distance_pairs)-1
-        #    print(f'add {pa}')
-        #else:
-            #print(f'skip {positions[i]} <--> {positions[shortest]}')
-    stop = time.time()
+        pairs += [(a, int(b), d) for b, d in zip(ki, distances)]
+    #stop = time.time()
     #print(f'Time : {stop-start:.3f}')
 
-    distance_pairs.sort(key=lambda x : x[2])
+    pairs.sort(key=lambda x : x[2])
 
-    # for i, (a, b, distance) in enumerate(distance_pairs):
-    #     print(f'{positions[a]} <--> {positions[b]} ({distance:.1f})')
-    #     if i == 10:
-    #         break
-    # return
-
-    #print(len(distance_pairs))
-
-    #circuits = {i : None for i in range(positions.shape[0])}
-
-    current_circuit_id = 0
     connections_count = 0
 
     circuits : Dict[int, set[int]]= {}
@@ -72,15 +44,16 @@ def main():
         return None
 
     current_new_id = 0
-    for a, b, distance in distance_pairs:
+    for a, b, distance in pairs:
         a_id = circuit_id(a)
         b_id = circuit_id(b)
 
         if a_id is not None and b_id is not None:
             # Move everything to a
-            circuits[a_id] |= circuits[b_id]
-            circuits.pop(b_id)
-            id = a_id
+            if a_id != b_id:
+                circuits[a_id] |= circuits[b_id]
+                circuits.pop(b_id)
+                id = a_id
         else:
             if a_id is not None:
                 id = a_id
@@ -95,34 +68,18 @@ def main():
             circuits[id].add(a)
             circuits[id].add(b)
 
-        # if circuits[a] is not None:
-        #     circuit_id = circuits[a]
-        # elif circuits[b] is not None:
-        #     circuit_id = circuits[b]
-        # else:
-        #     circuit_id = current_circuit_id
-        #     current_circuit_id += 1
-
-        # circuits[a] = circuit_id
-        # circuits[b] = circuit_id
-
-        print(f'{positions[a, :]} <--> {positions[b, :]} ({id})')
+        #print(f'{positions[a, :]} <--> {positions[b, :]} ({id})')
 
         connections_count += 1
-        if connections_count == N:
+        if connections_count == N_connections:
             break
+    
+    #print(circuits)
 
+    circuit_lengths = [len(lamps) for lamps in circuits.values()]
+    circuit_lengths.sort(reverse=True)
 
-    # circuit_sizes = {}
-    # for circuit in circuits.values():
-    #     if circuit is not None:
-    #         if circuit not in circuit_sizes:
-    #             circuit_sizes[circuit] = 0
-    #         circuit_sizes[circuit] += 1
-
-    # print(circuit_sizes.values())
-
-    print(circuits)
+    print(np.prod(circuit_lengths[:N_largest]))
 
 
 
---
+++ 2025/day08/day08_2.py
@@ -0,0 +1,79 @@
+from sys import argv
+from typing import Dict
+import numpy as np
+import time
+
+def main():
+    file = argv[1]
+
+    positions = []
+    with open(file) as f:
+        file_contents = f.read()
+
+        for line in file_contents.split('\n'):
+            positions.append(tuple(int(x) for x in line.split(',')))
+
+    positions = np.array(positions)
+
+    pairs = []
+
+    k = np.arange(positions.shape[0], dtype=int)
+
+    #start = time.time()
+    for a, p in enumerate(positions):
+        distances = np.linalg.norm(positions[a+1:,:] - p, axis=1)
+        ki = k[a+1:]
+
+        pairs += [(a, int(b), d) for b, d in zip(ki, distances)]
+    #stop = time.time()
+    #print(f'Time : {stop-start:.3f}')
+
+    pairs.sort(key=lambda x : x[2])
+
+    connections_count = 0
+
+    circuits : Dict[int, set[int]]= {}
+
+    def circuit_id(x : int):
+        for circuit_id, s in circuits.items():
+            if x in s:
+                return circuit_id
+        return None
+
+    current_new_id = 0
+    for a, b, distance in pairs:
+        a_id = circuit_id(a)
+        b_id = circuit_id(b)
+
+        if a_id is not None and b_id is not None:
+            # Move everything to a
+            if a_id != b_id:
+                circuits[a_id] |= circuits[b_id]
+                circuits.pop(b_id)
+                id = a_id
+        else:
+            if a_id is not None:
+                id = a_id
+            elif b_id is not None:
+                id = b_id
+            else:
+                id = current_new_id
+                current_new_id += 1
+            
+            if id not in circuits:
+                circuits[id] = set()
+            circuits[id].add(a)
+            circuits[id].add(b)
+
+        if len(circuits) == 1 and len(circuits[list(circuits.keys())[0]]) == positions.shape[0]:
+            #print(f'{positions[a, :]} <--> {positions[b, :]} ({id})')
+
+            print(positions[a, 0] * positions[b, 0])
+
+            break
+
+
+
+
+if __name__ == '__main__':
+    main()
