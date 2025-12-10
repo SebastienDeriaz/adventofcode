680dd50b2fcde02468f03db4949f11479a432be4
Sébastien Deriaz <sebastien.deriaz1@gmail.com>
2025-12-10 22:38:06 +0100

    sync

--- 2025/day10/day10_2.py
+++ 2025/day10/day10_2.py
@@ -3,6 +3,8 @@ from sys import argv
 import numpy as np
 from scipy.optimize import linprog
 import networkx as nx
+from itertools import combinations_with_replacement
+from functools import lru_cache
 
 def main():
 
@@ -21,98 +23,129 @@ def main():
                     N = len(lamp_string)
 
                 elif fragment.startswith('('):
-                    buttons.append([int(x) for x in fragment[1:-1].split(',')])
+                    buttons.append(tuple([int(x) for x in fragment[1:-1].split(',')]))
 
                 elif fragment.startswith('{'):
                     joltages = [int(x) for x in fragment[1:-1].split(',')]
                     assert len(joltages) == N
 
-            machines.append((N, lamps, buttons, joltages))
+            machines.append((N, lamps, tuple(buttons), joltages))
 
 
-    for N, _, buttons, joltages in machines[1:]:
+    total_presses = 0
+    for N, _, buttons, joltages in machines:
 
         print(f'Joltages : {joltages}')
+        print(f'Buttons : {buttons}')
 
-        #buttons = [(3,), (1,3), (2,3), (0,2), (0,1)]
+        buttons : list[int]
+
+        best_path = recurse_find_path(buttons, tuple(joltages), tuple([0]*len(buttons)))
+
+        print(f'Best path : {best_path}')
+        #print(joltages)
+        #calculated_joltages = calculate_joltage(len(joltages), buttons, best_path)
+        #print(valid_joltage(joltages, calculated_joltages))
+        #print(f'Best path : {best_path} {len(best_path)}')
 
 
+        #assert valid_joltage(joltages, calculated_joltages)
+
+        total_presses += sum(best_path)
+
+    print(total_presses)
+
+    return
+
+def combine_presses(a : dict[int, int], b : dict[int, int]):
+    assert len(a) == len(b)
+    #return {k : a.get(k, 0) + b.get(k, 0) for k in range(max(max(list(a)), max(list(b)))+1)}
+    return tuple([ai+bi for ai, bi in zip(a, b)])
+    
+def rc(n : int, values : list[int], n_buttons : int):
+    output : list[int] = [0]*n_buttons
+    #d = {v : 0 for v in values}
+    v = values[0]
+    if len(values) == 1:
+        output[v] += n
+        yield output
+        return None
+    
+    for j in range(0, n+1):
+        for rc_output in rc(n-j, values[1:], n_buttons):
+            di = output.copy()
+            di[v] += j
+            for i, counts in enumerate(rc_output):
+                di[i] += counts
+            yield di
+
+def calculate_joltage(N : int, buttons : list[tuple[int]], pressed_buttons : tuple[int]):
+    assert len(pressed_buttons) == len(buttons)
+    counters = [0]*N
+    for i, counts in enumerate(pressed_buttons):
+        for index in buttons[i]:
+            counters[index] += counts
+
+    return counters
 
 
-        # def counters_to_node(counters : list):
-        #     return '-'.join(str(c) for c in counters)
+def valid_joltage(reference_joltages : list[int], joltages : dict[int, int]):
+    return all(joltages[i] <= reference_joltages[i] for i in range(len(reference_joltages)))
+
+@lru_cache(maxsize=2048)
+def recurse_find_path(buttons : tuple[tuple[int]], joltages : tuple[int], button_presses : tuple[int], i : int = 0) -> list[int]:
+    N = len(joltages)
+    if i == len(joltages):
+        return button_presses
+    
+    # if joltages[i] == 0:
+    #     print('=0, go to next')
+    #     return recurse_find_path(buttons, joltages, path, i+1)
+
+    current_joltages = calculate_joltage(N, buttons, button_presses)
+    joltage_delta = joltages[i] - current_joltages[i]
+
+    if joltage_delta == 0:
+        #print(f'nothing to do on joltage {i}, go to next')
+        return recurse_find_path(buttons, joltages, button_presses, i+1)
+
+    available_buttons = [bi for bi, button in enumerate(buttons) if i in button]
+    
+    new_paths = []
+
+    #print(' '*i + f'Available buttons : {available_buttons} to get joltage {i} from {current_joltages[i]} to {joltages[i]}')
+
+
+    joltage_delta 
+
+    for presses in rc(joltage_delta, available_buttons, len(buttons)):
+        #print(' '*i + "  " + str(presses))
+        #print(' '*i + " +" + str(button_presses))
+        new_presses = combine_presses(button_presses, presses)
+        #print(' '*i + "c:" + str(new_presses))
+    #for path_suffix in combinations_with_replacement(available_buttons, joltage_delta):
+        #new_path = list(path) + list(path_suffix)
+        #print(path_suffix)
+        new_joltages = calculate_joltage(N, buttons, new_presses)
+
+        if not valid_joltage(joltages, new_joltages):
+            continue
+        #print(f'New path : {new_path} -> {new_joltages}')
+
+        #new_path.sort()
+        new_completed_path = recurse_find_path(buttons, joltages, new_presses, i+1)
+        if new_completed_path is not None:
+        #print(f'New path : {new_completed_path}')
+            new_paths.append(new_completed_path)
         
-        # g = nx.graph.Graph()
-        # for i, path in enumerate(itertools.product(enumerate(buttons), repeat=sum(joltages))):
-        #     counters = [0]*N
 
-        #     cs = counters_to_node(counters)
-        #     if not g.has_node(cs):
-        #         g.add_node(cs)
+    new_paths.sort(key=lambda path : sum(path))
+    #print(' '*i + f'Output paths : {new_paths}')
 
-        #     start_node = cs
-        #     for bi, b in path:
-        #         for ci in b:
-        #             counters[ci] += 1
-                
-        #         end_node = counters_to_node(counters)
-        #         if not g.has_node(cs):
-        #             g.add_node(cs)
-
-        #         g.add_edge(start_node, end_node, button_id=bi)
-
-
-        #     #print(path)
-
-
-        # #print(nx.shortest_path(g, counters_to_node([0]*N), counters_to_node(joltages)))
-
-        # print(g.nodes)
-
-        M = np.zeros((N, len(buttons)))
-
-        for bi, button in enumerate(buttons):
-            M[button, bi] = 1
-        print(M)
-
-        Mp = np.linalg.pinv(M)
-
-        #b_eq = np.array([2, 1, 1])
-        #c = np.zeros(len(buttons))
-        #bounds = [(0, max(joltages))] * len(buttons)
-
-        # res = linprog(c, A_eq=M, b_eq=joltages, bounds=bounds, method='highs-ipm')
-        # if res.success:
-        #     print(f'Presses : {res.x} ({sum(res.x)})')
-
-        #     print(M @ res.x)
-
-        #print(np.array([1, 3, 0, 3, 1, 2]).round(1))
-        print(Mp @ joltages)
-        print(f'Presses : {np.sum((Mp @ joltages))}')
-
-        #print(M @ np.array([1, 3, 0, 3, 1, 2]))
-        
-        # 1      3      0      3      1      2
-        # 1+7/11 3+1/11 0+7/11 2+3/11 1+1/11 1+10/11
-
-        # 7 -> floor
-        # 1 -> floor
-        # 7 -> floor
-        # 3 -> ceil
-        # 1 -> floor
-        # 10 -> ceil
-
-        2 
-        3 4 -1
-
-
-
-
-        break
-
-
-        #break
+    if len(new_paths) == 0:
+        return None
+    
+    return new_paths[0]
 
 if __name__ == '__main__':
     main()
